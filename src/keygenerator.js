bb43e3d4b00bc7f102745d5bbcd572f12b33919c
Ioanna Katsara <ionkatsara@gmail.com>
2021-08-10 20:56:01 +0300

    class of key generator, switching to Ganache

--- src/keygenerator.js
+++ src/keygenerator.js
@@ -5,13 +5,29 @@ const EC = require('elliptic').ec;
 const ec = new EC('secp256k1');
 
 // Generate a new key pair and convert them to hex-strings
-const key = ec.genKeyPair();
-const publicKey = key.getPublic('hex');
-const privateKey = key.getPrivate('hex');
+// const key = ec.genKeyPair();
+// const publicKey = key.getPublic('hex');
+// const privateKey = key.getPrivate('hex');
 
-// Print the keys to the console
-console.log();
-console.log('Your public key = ', publicKey);
+class Keys {
+    genKeyPair() {
+        // Generate a new key pair and convert them to hex-strings
+        const key = ec.genKeyPair();
+        const publicKey = key.getPublic('hex');
+        this.publicKey = publicKey;
+        const privateKey = key.getPrivate('hex');
+        this.privateKey = privateKey;
+        return [publicKey, privateKey];
+    }
 
-console.log();
-console.log('Your private key = ', privateKey);
+
+    // Print the keys to the console
+    printKeys() {
+        console.log();
+        console.log('Your public key = ', this.publicKey);
+        console.log('Your private key = ', this.privateKey);
+        console.log();
+    }
+}
+
+module.exports.Keys = Keys;
