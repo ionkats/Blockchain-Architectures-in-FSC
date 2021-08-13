d787b60bd9a2869d4775ba2ef9ddb8e847fca89d
Ioanna Katsara <ionkatsara@gmail.com>
2021-08-13 16:51:19 +0300

    connected,transactions with button,events handling

--- guiForGanache/farm.js
+++ guiForGanache/farm.js
@@ -1,66 +1,74 @@
-import contractAbi from "./ABI.js";
-console.log("YEE");
-let contractAddress;
-let contract;
-// window.web3 = new Web3(ethereum);
-// windows.contract = contract = new web3.methods.contract(contractAbi, contractAddress);
-// localStorage.setItem('contractAddress', contractAddress);
+import contractABI from "./ABI.js";
+let smartContractAddress = "0x812bB35f555a37c3895Cf563DDAEF071dCa5f193";
 
-const contractAddressButton = document.getElementById("contract-address");
-const startSessionButton = document.getElementById("newSession");
-const endSessionButton = document.getElementById("endSession");
-const HandoffButton = document.getElementById("Handoff");
-const sensorButton = document.getElementById("Sensor");
+const startSessionButton = document.getElementById("newSession-button");
+const endSessionButton = document.getElementById("endSession-button");
+const HandoffButton = document.getElementById("Handoff-button");
+const sensorButton = document.getElementById("Sensor-button");
+
+const web3 = new Web3 ("ws://localhost:7545");
+const smartContract = new web3.eth.Contract(contractABI, smartContractAddress);
+const senderAddress = "0x4F55a64D52c11B003415524c9a05ce812386FE5B";
 
 
-// See https://eips.ethereum.org/EIPS/eip-1102#eth_requestaccounts
-// and https://eips.ethereum.org/EIPS/eip-1193#request-1
-async function initEthereum () {
-    if (window.ethereum) {
-        window.web3 = new Web3(ethereum);
-        try {
-            oneAddress = (await ethereum.request({ method: 'eth_requestAccounts' }))[0];
-            // oneAddress = web3.utils.toChecksumAddress(playerAddress); // properly capitalize https://github.com/MetaMask/metamask-extension/issues/5826
-        } catch (e) {
-            console.error(e)
-            alert("No ethereum account provided.")
-        }
-        // document.getElementById("Account").value = playerAddress;
-    } else {
-        alert("No Ethereum provider (eg MetaMask) detected.");
-    }
-}
-initEthereum()
+let activeSessions = [];
+let numberOfActiveSessions;
+// dictionary key:sessionID, value: list of block hashes that it passed
+let sessionToBlockHash = {}; 
+// a queue of transaction data
+let transactionData = [];
+// var queue = [];
+// queue.push(2);         // queue is now [2]
+// queue.push(5);         // queue is now [2, 5]
+// var i = queue.shift(); // queue is now [5]
+// alert(i);
+
+listenAllEvents();
 
 
 function listenAllEvents() {
-    contract.events.StartOfSession(makeRelevantEventHandler(transactionEvent))
-    contract.events.SensorLog(makeRelevantEventHandler(transactionEvent))
-    contract.events.EndOfSession(makeRelevantEventHandler(transactionEvent))
-    contract.events.Handoff(makeRelevantEventHandler(transactionEvent))
+    smartContract.events.StartOfSession(specifiedEventHandler(startSessionEvent));
+    smartContract.events.Handoff(specifiedEventHandler(handoffEvent));
+    smartContract.events.SensorLog(specifiedEventHandler(sensorLogEvent));
+    smartContract.events.EndOfSession(specifiedEventHandler(endSessionEvent));
 }
 
 
-async function transactionEvent() {
-
-}
-
-
-async function connect() {
-    contractAddress = document.getElementById("contract-address-input").value;
-    if (!contractAddress) {
-        return;
+function specifiedEventHandler(handler) {
+    return function(error, result) {
+        if (error) {
+            console.error(error);
+            return;
+        }
+    
+        const values = result.returnValues;
+        transactionData.push(values);
+        handler(values);
+        console.log(values)
     }
-    window.contract = contract = new  web3.eth.Contract(contractAbi, contractAddress);
-    localStorage.setItem('contractAddress', contractAddress);
+}
 
-    listenAllEvents();
+async function startSessionEvent(values) {
+    activeSessions.push(values.sessionID);
+    numberOfActiveSessions += 1;
+}
+
+async function handoffEvent(values) {
+    sessionToBlockHash[values.sessionID].push(values.previousStateBlockHash);
+}
+
+async function sensorLogEvent(values) {
+    //do nothing?
+}
+
+async function endSessionEvent(result) {    
+    activeSessions.remove(values.sessionID);
+    numberOfActiveSessions -= 1;
 }
 
 
 function addUiListeners() {
     // specified transactions
-    contractAddressButton.addEventListener("click", getContractAddress);
     startSessionButton.addEventListener("click", startTransaction);
     endSessionButton.addEventListener("click", endTransaction);
     HandoffButton.addEventListener("click", handOffTransaction);
@@ -71,37 +79,43 @@ addUiListeners();
 
 async function getContractAddress() {
     var contractAddress = document.getElementById("contract-address-input");
+    isEmpty(contractAddress);
+    return contractAddress;
 }
 
 
-async function startTransaction() {
-    var _session = document.getElementById("newSession-sessionId").value;  
-    var _userId = document.getElementById("newSession-userId").value;
-    var _previousStateBlockHash = document.getElementById("newSession-previousStateBlockHash").value;
-    var _previousStateLedgerName = document.getElementById("newSession-previousStateLedgerName").value;
-    
-    isEmpty(_session);
-    isEmpty(_userId);
-    isEmpty(_previousStateBlockHash);
-    isEmpty(_previousStateLedgerName);
-
-    try {
-        startTransaction = contract.methods.StartSession(
-            _session,
-            _userId,
-            _previousStateBlockHash,
-            _previousStateLedgerName
-            ).send();
-    } catch (e) {
-        console.error(e);
-        alert(e.message);
-    }
-}
 function isEmpty(item) {
     if (item === null) {
         throw new Error("Empty Item on input");
     }
 }
+
+
+async function startTransaction() {
+    var _userId = document.getElementById("newSession-userId").value;
+    var _previousStateBlockHash = document.getElementById("newSession-previousStateBlockHash").value;
+    var _previousStateLedgerName = document.getElementById("newSession-previousStateLedgerName").value;
+    
+    isEmpty(_userId);
+    isEmpty(_previousStateBlockHash);
+    isEmpty(_previousStateLedgerName);
+
+    try {
+        startTransaction = smartContract.methods.startSession(
+            _userId,
+            _previousStateBlockHash,
+            _previousStateLedgerName
+            ).send({from: senderAddress});
+    } catch (e) {
+        console.error(e);
+        alert(e.message);
+    }
+
+    document.getElementById("newSession-userId").value = "";
+    document.getElementById("newSession-previousStateBlockHash").value = "";
+    document.getElementById("newSession-previousStateLedgerName").value = "";
+}
+
 
 async function handOffTransaction() {
     var _session = document.getElementById("Handoff-sessionId").value;  
@@ -117,17 +131,23 @@ async function handOffTransaction() {
     isEmpty(_previousStateLedgerName);
 
     try {
-        handOffTransaction = contract.methods.endStartSessionHandoff(
+        handOffTransaction = smartContract.methods.handoff(
             _session,
             _previousUserId,
             _newuserId,
             _previousStateBlockHash,
             _previousStateLedgerName
-            ).send();
+            ).send({from: senderAddress});
     } catch (e) {
         console.error(e);
         alert(e.message);
     }
+
+    document.getElementById("Handoff-sessionId").value = "";  
+    document.getElementById("Handoff-previousUserId").value = "";
+    document.getElementById("Handoff-newUserId").value = "";
+    document.getElementById("Handoff-previousStateBlockHash").value = "";
+    document.getElementById("Handoff-previousStateLedgerName").value = "";
 }
 
 
@@ -139,14 +159,17 @@ async function sensorTransaction() {
     isEmpty(_information);
 
     try {
-        sensorTransaction = contract.methods.sensorLogging(
+        sensorTransaction = smartContract.methods.sensorLogging(
             _session,
             _information
-            ).send();
+            ).send({from: senderAddress});
     } catch (e) {
         console.error(e);
         alert(e.message);
     }
+
+    document.getElementById("Sensor-sessionId").value = ""; 
+    document.getElementById("Sensor-information").value = ""; 
 }
 
 
@@ -158,14 +181,17 @@ async function endTransaction() {
     isEmpty(_userId);
 
     try {
-        endTransaction = contract.methods.endStartSessionHandoff(
+        endTransaction = smartContract.methods.endSession(
             _session,
             _userId
-            ).send();
+            ).send({from: senderAddress});
     } catch (e) {
         console.error(e);
         alert(e.message);
     }
+
+    document.getElementById("endSession-sessionId").value = "";
+    document.getElementById("endSession-userId").value = "";
 }
 
 
